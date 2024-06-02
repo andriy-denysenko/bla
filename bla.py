5bd59cf90a3b19d31bb6cf9ba41e608a85d29888
andriy-denysenko <denysenko.andriy@gmail.com>
2024-06-02 23:43:14 +0300

    Added logic to determine if the device is charging. Added 2 sound files for low and high battery. Refactored debug message into a separate file.

--- bla.py
+++ bla.py
@@ -1,24 +1,50 @@
 #!/usr/bin/env python3
 
-# Source: https://www.geeksforgeeks.org/create-battery-notifier-for-laptop-using-python/
-# Author abhisheksrivastaviot18
+# Based on: https://www.geeksforgeeks.org/create-battery-notifier-for-laptop-using-python/
+# By abhisheksrivastaviot18
 
 import psutil
 from plyer import notification
 from playsound import playsound
 import time
-import math
+import os
 
+from debug import print_debug_message
+
+# Debug flag
+IS_DEBUGGING = True
+
+# Default battery levels in percents
 MIN_PERCENT = 15
 MAX_PERCENT = 92
-SOUND = 'Alarm from Assignment - Earth.mp3'
-# TODO: add sound for low/high battery
-CHECK_INTERVAL = 3
+
+# Default check interval im minutes
+CHECK_INTERVAL = 0.5 #3
+
+# Sound files
+SOUND_BATTERY_LOW = os.path.join('res', 'mixkit-alert-alarm-1005.wav')
+SOUND_BATTERY_HIGH = os.path.join('res', 'alarm-no3-14864.mp3')
+
+# Get battery data
+battery = psutil.sensors_battery()
+
+# Set previous percent to current battery level
+prev_percent = battery.percent
+
+# Try to detect if the device is plugged
+is_charging = True if battery.power_plugged else False
 
 # TODO: store settings in the settings file
-#TODO: add tray icon and GUI for settings
+# TODO: add tray icon and GUI for settings
+
 
 def notify(text, sound_file):
+	"""
+	Shows tray notification and plays sound alarm
+	:param text: text to display
+	:param sound_file: sound alarm to play
+	:return: None
+	"""
 	notification.notify(
 		title="Battery Percentage",
 		message=text,
@@ -27,24 +53,31 @@ def notify(text, sound_file):
 
 	playsound(sound_file)
 
+
 while True:
+	# Refresh battery value each iteration
 	battery = psutil.sensors_battery()
+
+	# Get battery data into variables
 	percent = battery.percent
+	secs_left = battery.secsleft
+	is_plugged = battery.power_plugged
 
-	if percent <= MIN_PERCENT:
-		message = f'{math.floor(percent)}% battery remaining'
-		notify(message, SOUND)
-	
-	elif percent >= MAX_PERCENT:
-		message = f'{math.floor(percent)}% battery reached'
-		notify(message, SOUND)
+	if IS_DEBUGGING:
+		print_debug_message(percent, prev_percent, secs_left, is_plugged)
 
-	#TODO: Repeat alert if battery:
-	# continues to discharge under MIN_PERCENT
-	# continues to charge over MAX_PERCENT or stays at 100%
+	# Notify user of low battery
+	if percent < MIN_PERCENT and not is_plugged and not is_charging:
+		message = f'''Battery percent ({percent}%) is below minimum ({MAX_PERCENT}%).
+Please connect the power chord.'''
+		notify(message, SOUND_BATTERY_LOW)
+
+	# Notify user of high battery
+	elif (percent > MAX_PERCENT or percent == 100) and is_plugged and is_charging:
+		message = f'''Battery percent ({percent}%) exceeds maximum ({MAX_PERCENT}%).
+Please disconnect the power chord.'''
+		notify(message, SOUND_BATTERY_HIGH)
 	
 	# Check each CHECK_INTERVAL minutes
-	time.sleep(60*CHECK_INTERVAL)
-	
-	continue
+	time.sleep(60 * CHECK_INTERVAL)
 
---
+++ debug.py
@@ -0,0 +1,40 @@
+import math
+from datetime import timedelta
+import psutil
+
+
+def print_debug_message(percent, prev_percent, secs_left, is_plugged):
+    # Make a string for battery percent
+    percent_str = f'{math.floor(percent)}% battery'
+
+    # Make a string for seconds left
+    secs_left_str = str(secs_left)
+    if secs_left == psutil.POWER_TIME_UNKNOWN:
+        time_left_str = 'unknown'
+    elif secs_left == psutil.POWER_TIME_UNLIMITED:
+        time_left_str = 'unlimited'
+    else:
+        time_left_str = str(timedelta(seconds=secs_left))
+
+    # Try to detect if the device charges or the AC power cable is connected
+    if percent > prev_percent or is_plugged:
+        is_charging = True
+    else:
+        is_charging = False
+
+    # Make strings for a test message
+    is_charging_str = f'The device is {"not " if not is_charging else ""}charging'
+    power_plugged_str = f'and is {"not " if not is_plugged else ""}plugged'
+
+    # Make a test message
+    test_msg = f'''-------------
+BATTERY INFO:
+Percent: {percent}, time left: {time_left_str}
+{is_charging_str} {power_plugged_str}
+
+percent: {percent}
+secs_left: {secs_left}
+power_plugged: {is_plugged}
+'''
+
+    print(test_msg)
